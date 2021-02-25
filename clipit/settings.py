2b74d52060cb5a28fcbb17ad7626aa24d8c6268c
varunkmr19 <varun-kumar@live.com>
2021-02-25 16:43:25 +0530

    add models

--- clipit/settings.py
+++ clipit/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -111,3 +113,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL='core.User'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+  pass
+
+
+class Tag(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
+
+
+class Collection(models.Model):
+  name = models.CharField(max_length=255)
+  tags = models.ManyToManyField(Tag)
+  owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='collection')
+
+  def __str__(self):
+    return self.name
+
+
+class Bookmark(models.Model):
+  title = models.CharField(max_length=255)
+  url = models.TextField()
+  collection = models.ManyToManyField(Collection)
+
+  def __str__(self):
+    return self.title
