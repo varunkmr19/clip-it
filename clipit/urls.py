443824dc86800ce3c001748f0878003c560a0a7a
varunkmr19 <varun-kumar@live.com>
2021-04-17 18:00:16 +0530

    Core: Add Index View

--- clipit/urls.py
+++ clipit/urls.py
@@ -6,25 +6,30 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Clip-It API",
-      default_version='v1',
-      description="A lightwight bookmark manager",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="varun-kumar@live.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Clip-It API",
+        default_version='v1',
+        description="A lightwight bookmark manager",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="varun-kumar@live.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
-    path('api/authentication/', include('authentication.urls'), name='authentication'),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/authentication/', include('authentication.urls'),
+         name='authentication'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger',
+        cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc',
+        cache_timeout=0), name='schema-redoc'),
 
 ]
