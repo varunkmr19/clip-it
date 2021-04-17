9f327cfb18789224590d21103982e059c2336163
varunkmr19 <varun-kumar@live.com>
2021-04-17 18:25:03 +0530

    Core: Register UI

--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'core/index.html')
+
+
+def register_view(request):
+    return render(request, 'core/register.html')
