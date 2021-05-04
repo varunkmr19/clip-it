7133de69305374e85d259d40f607366d031ef273
varunkmr19 <varun-kumar@live.com>
2021-05-04 18:23:27 +0530

    Core: Create and Edit Collections

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
+    path('collection/create', views.create_collection_view,
+         name='create_collection'),
+    path('collection/edit/<int:collection_id>', views.edit_collection_view,
+         name='edit_collection'),
+    path('collection/delete/<int:collection_id>',
+         views.delete_collection, name='delete_collection'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('profile', views.profile_view, name='profile'),
--- core/views.py
+++ core/views.py
@@ -3,11 +3,20 @@ from django.db import IntegrityError
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from . models import Bookmark, Collection, Tag
 
 
+@login_required
 def index_view(request):
-    return render(request, 'core/index.html')
+    user = request.user
+    collections = Collection.objects.filter(owner=user)
+    bookmarks = Bookmark.objects.filter(owner=user)
+    return render(request, 'core/index.html', {
+        'bookmarks': bookmarks,
+        'collections': collections
+    })
 
 
 def login_view(request):
@@ -30,6 +39,7 @@ def login_view(request):
         return render(request, "core/login.html")
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("login"))
@@ -62,5 +72,48 @@ def register_view(request):
         return render(request, "core/register.html")
 
 
+@login_required
 def profile_view(request):
     return render(request, 'core/profile.html')
+
+
+@login_required
+def create_collection_view(request):
+    if request.method == 'POST':
+        name = request.POST['collection']
+        if not name:
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        user = request.user
+        collection = Collection.objects.create(name=name, owner=user)
+        collection.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    return render(request, 'core/create_collection.html')
+
+
+def edit_collection_view(request, collection_id):
+    try:
+        collection = Collection.objects.get(
+            owner=request.user, pk=collection_id)
+        context = {'collection': collection}
+    except Collection.DoesNotExist:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+    if request.method == 'POST':
+        name = request.POST['collection']
+        collection.name = name
+        collection.save()
+
+        return redirect('index')
+    return render(request, 'core/edit_collection.html', context)
+
+
+@login_required
+def delete_collection(request, collection_id):
+    try:
+        collection = Collection.objects.get(
+            owner=request.user, pk=collection_id)
+    except Collection.DoesNotExist:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    if request.method == 'POST':
+        collection.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
