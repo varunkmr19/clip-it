0f8bb31d69ce02a5d0da1506b41401c5ecb16276
varunkmr19 <varun-kumar@live.com>
2021-05-06 10:51:25 +0530

    Core: Shortcut Model

--- core/models.py
+++ core/models.py
@@ -29,3 +29,13 @@ class Bookmark(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Shortcut(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='shortcut')
+
+    def __str__(self):
+        return self.title
